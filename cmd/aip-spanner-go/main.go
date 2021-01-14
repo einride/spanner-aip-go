fc2542081b4c912bf265a06dd9bed369daedbef6
Oscar Söderlund <odsod@users.noreply.github.com>
2021-01-14 07:58:17 +0100

    feat: support generating multiple databases
    Generate an additional musicdb from the official Spanner examples. To be
    used for documentation and example code.

--- cmd/aip-spanner-go/main.go
+++ cmd/aip-spanner-go/main.go
@@ -34,87 +34,51 @@ func main() {
 		}
 	}()
 	var config struct {
-		SchemaGlobs []string `yaml:"schema"`
-		Package     struct {
-			Name string `yaml:"name"`
-			Path string `yaml:"path"`
-		} `yaml:"package"`
+		Databases []struct {
+			Name        string   `yaml:"name"`
+			SchemaGlobs []string `yaml:"schema"`
+			Package     struct {
+				Name string `yaml:"name"`
+				Path string `yaml:"path"`
+			} `yaml:"package"`
+		} `yaml:"databases"`
 	}
 	if err := yaml.NewDecoder(configFile).Decode(&config); err != nil {
 		log.Panic(err)
 	}
-	var db spanddl.Database
-	for _, schemaGlob := range config.SchemaGlobs {
-		schemaFiles, err := filepath.Glob(schemaGlob)
-		if err != nil {
+	for _, databaseConfig := range config.Databases {
+		var db spanddl.Database
+		for _, schemaGlob := range databaseConfig.SchemaGlobs {
+			schemaFiles, err := filepath.Glob(schemaGlob)
+			if err != nil {
+				log.Panic(err)
+			}
+			for _, schemaFile := range schemaFiles {
+				schema, err := ioutil.ReadFile(schemaFile)
+				if err != nil {
+					log.Panic(err)
+				}
+				ddl, err := spansql.ParseDDL(schemaFile, string(schema))
+				if err != nil {
+					log.Panic(err)
+				}
+				if err := db.ApplyDDL(ddl); err != nil {
+					log.Panic(err)
+				}
+			}
+		}
+		if err := os.MkdirAll(databaseConfig.Package.Path, 0o775); err != nil {
 			log.Panic(err)
 		}
-		for _, schemaFile := range schemaFiles {
-			schema, err := ioutil.ReadFile(schemaFile)
-			if err != nil {
-				log.Panic(err)
-			}
-			ddl, err := spansql.ParseDDL(schemaFile, string(schema))
-			if err != nil {
-				log.Panic(err)
-			}
-			if err := db.ApplyDDL(ddl); err != nil {
-				log.Panic(err)
-			}
-		}
-	}
-	if err := os.MkdirAll(config.Package.Path, 0o775); err != nil {
-		log.Panic(err)
-	}
-	filename := filepath.Join(config.Package.Path, "descriptor_gen.go")
-	f := codegen.NewFile(codegen.FileConfig{
-		Filename:    filename,
-		Package:     config.Package.Name,
-		GeneratedBy: generatedBy,
-	})
-	descriptorcodegen.DatabaseDescriptorCodeGenerator{
-		Database: &db,
-	}.GenerateCode(f)
-	content, err := f.Content()
-	if err != nil {
-		log.Panic(err)
-	}
-	if err := ioutil.WriteFile(filename, content, 0o600); err != nil {
-		log.Panic(err)
-	}
-	log.Println("wrote:", filename)
-	if len(db.Tables) > 0 {
-		filename := filepath.Join(config.Package.Path, "tables_gen.go")
+		filename := filepath.Join(databaseConfig.Package.Path, "descriptor_gen.go")
 		f := codegen.NewFile(codegen.FileConfig{
 			Filename:    filename,
-			Package:     config.Package.Name,
+			Package:     databaseConfig.Package.Name,
 			GeneratedBy: generatedBy,
 		})
-		for _, table := range db.Tables {
-			tablecodegen.ReadTransactionCodeGenerator{Table: table}.GenerateCode(f)
-			tablecodegen.RowIteratorCodeGenerator{Table: table}.GenerateCode(f)
-			tablecodegen.PrimaryKeyCodeGenerator{Table: table}.GenerateCode(f)
-			tablecodegen.PartialKeyCodeGenerator{Table: table}.GenerateCode(f)
-			tablecodegen.KeyRangeCodeGenerator{Table: table}.GenerateCode(f)
-			tablecodegen.RowCodeGenerator{Table: table}.GenerateCode(f)
-			interleavedTables := db.InterleavedTables(table.Name)
-			if len(interleavedTables) == 0 {
-				continue
-			}
-			tablecodegen.InterleavedReadTransactionCodeGenerator{
-				Table:             table,
-				InterleavedTables: interleavedTables,
-			}.GenerateCode(f)
-			tablecodegen.InterleavedRowIteratorCodeGenerator{
-				Table:             table,
-				InterleavedTables: interleavedTables,
-			}.GenerateCode(f)
-			tablecodegen.InterleavedRowCodeGenerator{
-				Table:             table,
-				InterleavedTables: interleavedTables,
-			}.GenerateCode(f)
-		}
-		tablecodegen.CommonCodeGenerator{}.GenerateCode(f)
+		descriptorcodegen.DatabaseDescriptorCodeGenerator{
+			Database: &db,
+		}.GenerateCode(f)
 		content, err := f.Content()
 		if err != nil {
 			log.Panic(err)
@@ -123,5 +87,46 @@ func main() {
 			log.Panic(err)
 		}
 		log.Println("wrote:", filename)
+		if len(db.Tables) > 0 {
+			filename := filepath.Join(databaseConfig.Package.Path, "tables_gen.go")
+			f := codegen.NewFile(codegen.FileConfig{
+				Filename:    filename,
+				Package:     databaseConfig.Package.Name,
+				GeneratedBy: generatedBy,
+			})
+			for _, table := range db.Tables {
+				tablecodegen.ReadTransactionCodeGenerator{Table: table}.GenerateCode(f)
+				tablecodegen.RowIteratorCodeGenerator{Table: table}.GenerateCode(f)
+				tablecodegen.PrimaryKeyCodeGenerator{Table: table}.GenerateCode(f)
+				tablecodegen.PartialKeyCodeGenerator{Table: table}.GenerateCode(f)
+				tablecodegen.KeyRangeCodeGenerator{Table: table}.GenerateCode(f)
+				tablecodegen.RowCodeGenerator{Table: table}.GenerateCode(f)
+				interleavedTables := db.InterleavedTables(table.Name)
+				if len(interleavedTables) == 0 {
+					continue
+				}
+				tablecodegen.InterleavedReadTransactionCodeGenerator{
+					Table:             table,
+					InterleavedTables: interleavedTables,
+				}.GenerateCode(f)
+				tablecodegen.InterleavedRowIteratorCodeGenerator{
+					Table:             table,
+					InterleavedTables: interleavedTables,
+				}.GenerateCode(f)
+				tablecodegen.InterleavedRowCodeGenerator{
+					Table:             table,
+					InterleavedTables: interleavedTables,
+				}.GenerateCode(f)
+			}
+			tablecodegen.CommonCodeGenerator{}.GenerateCode(f)
+			content, err := f.Content()
+			if err != nil {
+				log.Panic(err)
+			}
+			if err := ioutil.WriteFile(filename, content, 0o600); err != nil {
+				log.Panic(err)
+			}
+			log.Println("wrote:", filename)
+		}
 	}
 }
