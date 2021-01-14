0637da2a61a2d29c2dd968ad44ac76a871e6cd1d
Oscar Söderlund <odsod@users.noreply.github.com>
2021-01-14 18:58:20 +0100

    chore: introduce top-level tablecodegen.DatabaseCodeGenerator
    Generates code for a full database.

---
+++ internal/codegen/tablecodegen/database_test.go
@@ -0,0 +1,15 @@
+package tablecodegen
+
+import (
+	"testing"
+
+	"go.einride.tech/aip-spanner/internal/codegen"
+	"go.einride.tech/aip-spanner/spanddl"
+)
+
+func TestDatabaseCodeGenerator_GenerateCode(t *testing.T) {
+	t.Parallel()
+	runGoldenFileTest(t, "database", func(db *spanddl.Database, f *codegen.File) {
+		DatabaseCodeGenerator{Database: db}.GenerateCode(f)
+	})
+}
