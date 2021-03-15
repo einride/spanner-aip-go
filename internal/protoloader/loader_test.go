72ddec03e051d0149e04772d8f35803657bde0f2
ericwenn <ericwenn@users.noreply.github.com>
2021-03-15 18:38:37 +0100

    chore: bump dependencies

--- internal/protoloader/loader_test.go
+++ internal/protoloader/loader_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestLoadFilesFromGoPackage(t *testing.T) {
 	t.Parallel()
-	files, err := LoadFilesFromGoPackage("go.einride.tech/aip/examples/proto/gen/einride/example/freight/v1")
+	files, err := LoadFilesFromGoPackage("go.einride.tech/aip/proto/gen/einride/example/freight/v1")
 	assert.NilError(t, err)
 	assert.Assert(t, files.NumFiles() > 0)
 }
