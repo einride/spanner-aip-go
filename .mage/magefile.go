69648c299b3f266b188ffe88c55b3d2faeb8eb84
Viktor Voltaire <viktor.voltaire@einride.tech>
2022-01-13 11:25:13 +0100

    chore: bump mage-tools

--- .mage/magefile.go
+++ .mage/magefile.go
@@ -6,6 +6,8 @@ package main
 import (
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
+	"go.einride.tech/mage-tools/mgmake"
+	"go.einride.tech/mage-tools/mgpath"
 
 	// mage:import
 	"go.einride.tech/mage-tools/targets/mgconvco"
@@ -23,6 +25,15 @@ import (
 	"go.einride.tech/mage-tools/targets/mggitverifynodiff"
 )
 
+func init() {
+	mgmake.GenerateMakefiles(
+		mgmake.Makefile{
+			Path:          mgpath.FromGitRoot("Makefile"),
+			DefaultTarget: All,
+		},
+	)
+}
+
 func All() {
 	mg.Deps(
 		mg.F(mgconvco.ConvcoCheck, "origin/master..HEAD"),
@@ -31,12 +42,12 @@ func All() {
 	)
 	mg.SerialDeps(
 		mggo.GoTest,
-    SpannerGenerate,
+		SpannerGenerate,
 		mggo.GoModTidy,
 		mggitverifynodiff.GitVerifyNoDiff,
 	)
 }
 
 func SpannerGenerate() error {
-  return sh.RunV("go", "run", ".", "generate")
+	return sh.RunV("go", "run", ".", "generate")
 }
