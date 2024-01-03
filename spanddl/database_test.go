33c5c38c1a76612f74b187504e6caf70fdcc62e7
Niclas Thall <niclas.thall@einride.tech>
2024-01-03 16:45:38 +0100

    fix(spanddl): support dropping interleaved tables
    Before the parent would have an reference to the dropped table, which
    caused an type error since the type could not be resolved.
    
    This PR removes the child reference in the parent table.

--- spanddl/database_test.go
+++ spanddl/database_test.go
@@ -293,6 +293,46 @@ func TestDatabase_ApplyDDL(t *testing.T) {
 		},
 
 		{
+			name: "drop interleaved",
+			ddls: []string{
+				`CREATE TABLE Singers (
+				  SingerId   INT64 NOT NULL,
+				  FirstName  STRING(1024),
+				  LastName   STRING(1024),
+				  SingerInfo BYTES(MAX),
+				  BirthDate  DATE,
+				) PRIMARY KEY(SingerId);`,
+
+				`CREATE TABLE Albums (
+				  SingerId     INT64 NOT NULL,
+				  AlbumId      INT64 NOT NULL,
+				  AlbumTitle   STRING(MAX),
+				) PRIMARY KEY (SingerId, AlbumId),
+				  INTERLEAVE IN PARENT Singers ON DELETE CASCADE;`,
+
+				`DROP TABLE Albums;`,
+			},
+			expected: &Database{
+				Tables: []*Table{
+					{
+						Name: "Singers",
+						Columns: []*Column{
+							{Name: "SingerId", Type: spansql.Type{Base: spansql.Int64}, NotNull: true},
+							{Name: "FirstName", Type: spansql.Type{Base: spansql.String, Len: 1024}},
+							{Name: "LastName", Type: spansql.Type{Base: spansql.String, Len: 1024}},
+							{Name: "SingerInfo", Type: spansql.Type{Base: spansql.Bytes, Len: spansql.MaxLen}},
+							{Name: "BirthDate", Type: spansql.Type{Base: spansql.Date}},
+						},
+						PrimaryKey: []spansql.KeyPart{
+							{Column: "SingerId"},
+						},
+						InterleavedTables: []*Table{},
+					},
+				},
+			},
+		},
+
+		{
 			name: "drop table with interleaved tables",
 			ddls: []string{
 				`CREATE TABLE Singers (
